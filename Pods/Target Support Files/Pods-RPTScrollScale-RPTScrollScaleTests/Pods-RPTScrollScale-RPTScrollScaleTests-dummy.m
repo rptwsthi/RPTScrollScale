b35d02395a6557037f13329ee673915f751b8975
Arpit <Arpit@technologies33.com>
2020-01-09 17:31:07 +0530

    first version of scroll scale view.

---
+++ Pods/Target Support Files/Pods-RPTScrollScale-RPTScrollScaleTests/Pods-RPTScrollScale-RPTScrollScaleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RPTScrollScale_RPTScrollScaleTests : NSObject
+@end
+@implementation PodsDummy_Pods_RPTScrollScale_RPTScrollScaleTests
+@end
---
+++ RPTScrollScale/RPTScrollScale.h
@@ -0,0 +1,19 @@
+//
+//  RPTScrollScale.h
+//  RPTScrollScale
+//
+//  Created by Arpit on 1/9/20.
+//  Copyright © 2020 Arpit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RPTScrollScale.
+FOUNDATION_EXPORT double RPTScrollScaleVersionNumber;
+
+//! Project version string for RPTScrollScale.
+FOUNDATION_EXPORT const unsigned char RPTScrollScaleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPTScrollScale/PublicHeader.h>
+
+
